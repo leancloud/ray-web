5b8e157582bce2efbf9fcac7dd7e2cacd5452483
Tianyong Tang <tang3w@gmail.com>
2018-08-10 14:39:32 +0800

    Show full message information.

--- share/src/Message.js
+++ share/src/Message.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react'
+import ResizableIframe from './ResizableIframe'
 
 const Subject = (props) => {
-  return <h1>{props.subject}</h1>
+  return (
+    <header className="subject-box">
+      <span className="subject">{props.text}</span>
+    </header>
+  )
 }
 
-const Participant = (props) => {
-  return <div></div>
+const Address = (props) => {
+  const name = props.address.name
+  const email = props.address.email
+  const mailto = `mailto:${email}`
+
+  return (
+    <span className="address-box">
+      <span className="address-name">{name}</span>
+      <a className="address-email" href={mailto}>&lt;{email}&gt;</a>
+    </span>
+  )
+}
+
+const Summary = (props) => {
+  const { from, to, date } = props
+
+  const time = new Date(date * 1000)
+  const dateNode = <span className="summary-date">{time.toLocaleString()}</span>
+  const fromNodeList = from.map(address => <Address key={address.email} address={address} />)
+  const toNodeList = to.map(address => <Address key={address.email} address={address} />)
+
+  return (
+    <section className="summary-box">
+      <section className="summary-top-box">
+        <div className="summary-from-box">{fromNodeList}</div>
+        <div className="summary-date-box">{dateNode}</div>
+      </section>
+      <section className="summary-to-box">
+        <span className="summary-to-label">To:</span>
+        {toNodeList}
+      </section>
+    </section>
+  )
 }
 
 const Content = (props) => {
-  return <div></div>
+  const html = props.html
+
+  return (
+    <section className="content-box">
+      <ResizableIframe className="content-iframe" srcDoc={html} />
+    </section>
+  )
 }
 
 class Message extends Component {
 
   render() {
-    let data = this.props.data
+    const data = this.props.data
 
     return (
-      <div className="message">
-        <Subject subject={data.subject} />
-        <Participant from={data.from} to={data.to} date={data.date} />
-        <Content body={data.body} />
-      </div>
+      <section className="message-box">
+        <Subject text={data.subject} />
+        <Summary from={data.from} to={data.to} date={data.date} />
+        <Content html={data.body} />
+      </section>
     )
   }
 
---
+++ share/src/ResizableIframe.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class ResizableIframe extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      height: '0px'
+    }
+  }
+
+  render() {
+    return (
+      <iframe
+        style={{
+          width: "100%",
+          height: this.state.height,
+          minWidth: "100%",
+          overflow: "visible"
+        }}
+        onLoad={() => {
+          const iframe = ReactDOM.findDOMNode(this)
+          this.setState({
+            height: iframe.contentWindow.document.body.scrollHeight + "px"
+          })
+        }}
+        ref="iframe"
+        srcDoc={this.props.srcDoc}
+        width="100%"
+        height={this.state.height}
+        frameBorder="0"
+        scrolling="no"
+      />
+    )
+  }
+
+}
+
+export default ResizableIframe
