dc12291b83064ab0c14e1f252eb0995095db799b
Tianyong Tang <tang3w@gmail.com>
2018-08-10 15:40:12 +0800

    Add css for message sharing page.

--- share/src/index.js
+++ share/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './index.css'
+
 import Request from './Request'
 import Message from './Message'
 import Failure from './Failure'
