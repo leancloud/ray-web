776f27826c0fed7c13f98a24ebd83f5041f7f7f3
Tianyong Tang <tang3w@gmail.com>
2018-08-22 16:20:13 +0800

    :sparkles: Move some metadata to index.html

--- src/Message.js
+++ src/Message.js
@@ -82,11 +82,8 @@ class Message extends Component {
         <section className="message-box">
           <Helmet>
             <title>Ray - {subject}</title>
-            <meta property="og:type" content="website" />
             <meta property="og:title" content={subject} />
-            <meta property="og:description" content="A message shared from Ray" />
             <meta property="og:url" content={window.location.href} />
-            <meta property="og:image" content="https://share.hiray.io/icon.png" />
           </Helmet>
           <h2 className="ui header">{subject}</h2>
           <Summary from={from} to={to} date={date} />
